659ac6a467ba2168969c98fc0271c43b4f5d025d
gomathi-ms <s567089@nwmissouri.edu>
2023-11-29 22:52:48 -0600

    login

--- app.js
+++ app.js
@@ -1,43 +1,73 @@
-
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-const connectionString =
-process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString,
-{useNewUrlParser: true,
-useUnifiedTopology: true});
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-// passport config
-// Use the existing connection
-// The Account model
-var Account =require('./models/account');
+
+const Account = require('./models/account');
+const Tea = require('./models/Tea');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const TeaRouter = require('./routes/Tea');
+const boardRouter = require('./routes/board');
+const chooseRouter = require('./routes/choose');
+const resourceRouter = require('./routes/resource');
+
+const app = express();
+
+// Passport configuration
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
  
-var Tea = require("./models/Tea");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var TeaRouter = require('./routes/Tea');
-var boardRouter = require('./routes/board');
-var chooseRouter = require('./routes/choose');
-var resourceRouter= require('./routes/resource');
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+ 
+        // Use the authenticate method provided by passport-local-mongoose
+        user.authenticate(password, function (err, isValid) {
+          if (err) {
+            return done(err);
+          }
+         
+          if (!isValid) {
+            return done(null, false, { message: 'Incorrect password.' });
+          }
+         
+          return done(null, user);
+        });
+      })
+      .catch(function (err) {
+        return done(err);
+      });
+  }
+));
 
-var app = express();
 
-// view engine setup
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -52,80 +82,50 @@ app.use('/users', usersRouter);
 app.use('/Tea', TeaRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
-app.use('/resource',resourceRouter);
+app.use('/resource', resourceRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-async function recreateDB(){
-  // Delete everything
+const connectionString = process.env.MONGO_CON;
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log('Connection to DB succeeded');
+});
+
+async function recreateDB() {
   await Tea.deleteMany();
-    let instance1 = new Tea({Tea_flavour:"Black Tea", Tea_cost:50.00,Tea_quantity:15});
-    let instance2 = new Tea({Tea_flavour:"Green Ta", Tea_cost:80.00,Tea_quantity:20});
-    let instance3 = new Tea({Tea_flavour:"Cold Tea", Tea_cost:100.00,Tea_quantity:30});
-    instance1.save().then(doc=>{
-    console.log("First object saved")}
-    ).catch(err=>{
-    console.error(err)
-    });
-    instance2.save().then(doc=>{
-      console.log("Second object saved")}
-      ).catch(err=>{
-      console.error(err)
-      });
-    instance3.save().then(doc=>{
-        console.log("Third object saved")}
-        ).catch(err=>{
-        console.error(err)
-        });
-    }
-    let reseed = true;
-    if (reseed) {recreateDB();}
-    app.use(require('express-session')({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false
-     }));
-     app.use(passport.initialize());
-     app.use(passport.session());  
- // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const teas = [
+    { Tea_flavour: 'Black Tea', Tea_cost: 50.00, Tea_quantity: 15 },
+    { Tea_flavour: 'Green Tea', Tea_cost: 80.00, Tea_quantity: 20 },
+    { Tea_flavour: 'Cold Tea', Tea_cost: 100.00, Tea_quantity: 30 }
+  ];
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-    Account.findOne({ username: username })
-    .then(function (user){
-    if (err) { return done(err); }
-    if (!user) {
-    return done(null, false, { message: 'Incorrect username.' });
+  for (const teaData of teas) {
+    const tea = new Tea(teaData);
+    try {
+      await tea.save();
+      console.log('Tea object saved:', teaData);
+    } catch (err) {
+      console.error(err);
     }
-    if (!user.validPassword(password)) {
-    return done(null, false, { message: 'Incorrect password.' });
-    }
-    return done(null, user);
-    })
-    .catch(function(err){
-    return done(err)
-    })
-    })
-    )
-    app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
+  }
+}
 
-  module.exports = app;
+const reseed = true;
+if (reseed) {
+  recreateDB();
+}
 
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
