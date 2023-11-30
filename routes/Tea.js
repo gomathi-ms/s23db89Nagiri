46585f083ab4d10fbaaa6d85d29f67c38dfb06a8
gomathi-ms <s567089@nwmissouri.edu>
2023-11-29 23:12:06 -0600

    view and delete

--- routes/Tea.js
+++ routes/Tea.js
@@ -3,12 +3,20 @@ const Tea_controlers= require('../controllers/Tea');
 var router = express.Router();
 
 /* GET home page. */
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 
-router.get('/', Tea_controlers.Tea_view_all_Page );
-router.get('/detail', Tea_controlers.Tea_view_one_Page);
-router.get('/create', Tea_controlers.Tea_create_Page);
-router.get('/update', Tea_controlers.Tea_update_Page);
-router.get('/delete', Tea_controlers.Tea_delete_Page);
+    
+
+router.get('/', secured, Tea_controlers.Tea_view_all_Page );
+router.get('/detail', secured,Tea_controlers.Tea_view_one_Page);
+router.get('/create',secured, Tea_controlers.Tea_create_Page);
+router.get('/update', secured,Tea_controlers.Tea_update_Page);
+router.get('/delete', secured,Tea_controlers.Tea_delete_Page);
 
 module.exports = router;
 
